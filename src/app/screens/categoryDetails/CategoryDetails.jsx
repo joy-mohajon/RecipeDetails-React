fdfb1b10c593e87b4b2b50e3a4fc0e72ad196394
mohajon33 <mohajon33@gmail.com>
2022-06-11 12:51:36 +0600

    add categoryDetails component

---
+++ src/app/screens/categoryDetails/CategoryDetails.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CategoryDetails = () => {
+  return (
+    <div>
+      <h6>$90 Menu for 4 People (Italian)</h6>
+      <p>Base Price: $360 ($90 per person)</p>
+      <p>Tax: $31.95 (8.875%)</p>
+      <p>Total Price: $391.95</p>
+    </div>
+  );
+};
+
+export default CategoryDetails;
